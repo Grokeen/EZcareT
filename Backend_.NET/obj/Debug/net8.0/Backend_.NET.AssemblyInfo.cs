8ba6e6acf586f0e6a55ec0d336d2806a3dc1529c
Grokeen <ygreen0516@gmail.com>
2024-06-29 23:12:42 +0900

    pull test

--- Backend_.NET/obj/Debug/net8.0/Backend_.NET.AssemblyInfo.cs
+++ Backend_.NET/obj/Debug/net8.0/Backend_.NET.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend_.NET")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fc3ce27873bf9e281124cfd21863ad477c12951")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+567f656576ad74335d724a8b17eeab9882b9dfb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend_.NET")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend_.NET")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
