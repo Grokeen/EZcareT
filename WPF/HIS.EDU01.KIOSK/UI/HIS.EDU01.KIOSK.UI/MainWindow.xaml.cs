dc78a19020780a056ec2e150ada71a4fd6cf08a0
inEZ <ygreen0516@gmail.com>
2024-07-03 17:58:35 +0900

    kiosk

---
+++ WPF/HIS.EDU01.KIOSK/UI/HIS.EDU01.KIOSK.UI/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using HIS.UI.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HIS.EDU01.KIOSK.UI
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : WindowBase
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
