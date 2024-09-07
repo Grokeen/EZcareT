f078cefbf75a3758c9dcb3cb62e03c447c539ab1
Grokeen <ygreen0516@gmail.com>
2024-09-07 19:57:22 +0900

    dd

---
+++ 보라매SI/SQL/240906_재강책임님_찾기.sql
@@ -0,0 +1,39 @@
+﻿SELECT *
+  FROM ALL_SOURCE
+;
+
+SELECT OWNER
+   , DBA_NAME
+         ,TYPE
+         ,LINE
+         ,COUNT(*) CNT            FROM (
+        SELECT
+         OWNER
+         ,NAME AS DBA_NAME
+         ,TYPE
+         ,LINE
+         ,TEXT
+      FROM
+         ALL_SOURCE
+         --DBA_SOURCE
+      WHERE UPPER(TEXT) LIKE '%APIPRSVT%'
+         AND UPPER(TEXT) LIKE '%RMK%'
+
+      UNION ALL
+
+        SELECT
+         OWNER
+         ,NAME AS DBA_NAME
+         ,TYPE
+         ,LINE
+         ,TEXT
+      FROM
+         --ALL_SOURCE
+         DBA_SOURCE
+      WHERE UPPER(TEXT) LIKE '%APOPRSVT%'
+         AND UPPER(TEXT) LIKE '%TEL_TALK%'   )
+         GROUP BY
+         OWNER
+   , DBA_NAME
+         ,TYPE
+         ,LINE
